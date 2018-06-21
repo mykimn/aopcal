9dcd026688329650e75fbc8cff9b56e380a9548b
Minyoung Kim <carat_dvl12@naver.com>
2018-06-21 21:06:42 +0900

    실행시간 측정하기 4 - 프록시 객체로 시간 측정

--- src/main/java/kr/hs/emirim/mykimn/aopcal/main/MainStart.java
+++ src/main/java/kr/hs/emirim/mykimn/aopcal/main/MainStart.java
@@ -9,7 +9,7 @@ public class MainStart {
 	public static void main(String[] args) {
 
 		Calculator cal1 = new ImpCalculator();
-		long f1 = cal1.factorial(100);
+		long f1 = cal1.factorial(4);
 		System.out.println("Imp 100 : " + f1);
 		
 		Calculator cal2 = new RecCalculator();
